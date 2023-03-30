90c09b9dce5ed72923bbe113badd8f1e5b7f4e53
npena535 <npena535@mtroyal.ca>
2023-03-30 16:24:39 -0600

    figured css path file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/views'));
 
 const posts = [
     {title: 'Title 1', body: 'Body 1' },
